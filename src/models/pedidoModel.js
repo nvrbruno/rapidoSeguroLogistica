7cde4ee611e345b52d41bda6df842d8afff8a56e
unknown <brunonavarrodavi@gmail.com>
2025-11-25 12:17:10 -0300

    pedidos finalizado sem comentario

--- src/models/pedidoModel.js
+++ src/models/pedidoModel.js
@@ -43,27 +43,27 @@ SELECT * FROM Pedidos
     }
   },
 
-inserirPedido: async (
-  idCliente,
-  dataPedido,
-  tipoPedido,
-  distanciaPedido,
-  pesoPedido,
-  valorBaseKm,
-  valorBaseKg,
-  valorTotal,
-  valorDistancia,
-  valorPeso,
-  acrescEntrega,
-  descEntrega,
-  taxaExtra
-) => {
-  const pool = await getConnection();
-  const transaction = new sql.Transaction(pool);
-  await transaction.begin();
+  inserirPedido: async (
+    idCliente,
+    dataPedido,
+    tipoPedido,
+    distanciaPedido,
+    pesoPedido,
+    valorBaseKm,
+    valorBaseKg,
+    valorTotal,
+    valorDistancia,
+    valorPeso,
+    acrescEntrega,
+    descEntrega,
+    taxaExtra
+  ) => {
+    const pool = await getConnection();
+    const transaction = new sql.Transaction(pool);
+    await transaction.begin();
 
-  try {
-    const querySQL = `
+    try {
+      const querySQL = `
       INSERT INTO Pedidos (
         idCliente,
         dataPedido,
@@ -87,68 +87,66 @@ inserirPedido: async (
       )
     `;
 
-    const result = await transaction
-      .request()
-      .input("idCliente", sql.UniqueIdentifier, idCliente)
-      .input("dataPedido", sql.Date, dataPedido)
-      .input("tipoPedido", sql.VarChar(7), tipoPedido)
-      .input("distanciaPedido", sql.Decimal(10, 2), distanciaPedido)
-      .input("pesoPedido", sql.Decimal(10, 2), pesoPedido)
-      .input("valorBaseKm", sql.Decimal(10, 2), valorBaseKm)
-      .input("valorBaseKg", sql.Decimal(10, 2), valorBaseKg)
-      .input("valorTotal", sql.Decimal(10, 2), valorTotal)
-      .query(querySQL);
+      const result = await transaction
+        .request()
+        .input("idCliente", sql.UniqueIdentifier, idCliente)
+        .input("dataPedido", sql.Date, dataPedido)
+        .input("tipoPedido", sql.VarChar(7), tipoPedido)
+        .input("distanciaPedido", sql.Decimal(10, 2), distanciaPedido)
+        .input("pesoPedido", sql.Decimal(10, 2), pesoPedido)
+        .input("valorBaseKm", sql.Decimal(10, 2), valorBaseKm)
+        .input("valorBaseKg", sql.Decimal(10, 2), valorBaseKg)
+        .input("valorTotal", sql.Decimal(10, 2), valorTotal)
+        .query(querySQL);
 
-    const idPedido = result.recordset[0].idPedido;
+      const idPedido = result.recordset[0].idPedido;
 
-    const querySQLEntregas = `
+      const querySQLEntregas = `
       INSERT INTO Entregas (idEntrega, idPedido, valorDistancia, valorPeso, acrescEntrega, descEntrega, taxaExtra, valorFinal)
       VALUES(@idEntrega, @idPedido, @valorDistancia, @valorPeso, @acrescEntrega, @descEntrega, @taxaExtra, @valorFinal )
     `;
 
-    await transaction
-      .request()
-      .input("idEntrega", sql.UniqueIdentifier, idCliente)
-      .input("idPedido", sql.UniqueIdentifier, idPedido)
-      .input("valorDistancia", sql.Decimal(10, 2), valorDistancia)
-      .input("valorPeso", sql.Decimal(10, 2), valorPeso)
-      .input("acrescEntrega", sql.Decimal(10, 2), acrescEntrega)
-      .input("descEntrega", sql.Decimal(10, 2), descEntrega)
-      .input("taxaExtra", sql.Decimal(10, 2), taxaExtra)
-      .input("valorFinal", sql.Decimal(10, 2), valorTotal)
-      .query(querySQLEntregas);
+      await transaction
+        .request()
+        .input("idEntrega", sql.UniqueIdentifier, idCliente)
+        .input("idPedido", sql.UniqueIdentifier, idPedido)
+        .input("valorDistancia", sql.Decimal(10, 2), valorDistancia)
+        .input("valorPeso", sql.Decimal(10, 2), valorPeso)
+        .input("acrescEntrega", sql.Decimal(10, 2), acrescEntrega)
+        .input("descEntrega", sql.Decimal(10, 2), descEntrega)
+        .input("taxaExtra", sql.Decimal(10, 2), taxaExtra)
+        .input("valorFinal", sql.Decimal(10, 2), valorTotal)
+        .query(querySQLEntregas);
 
-    await transaction.commit();
-  } catch (error) {
-    await transaction.rollback();
-    console.error("Erro ao inserir pedido:", error);
-    throw new Error("Erro ao inserir pedido no banco de dados.");
-  }
-},
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      console.error("Erro ao inserir pedido:", error);
+      throw new Error("Erro ao inserir pedido no banco de dados.");
+    }
+  },
 
-
-atualizarPedido: async (
-        idPedido,
-        idClienteAtualizado,
-        dataPedidoAtualizado,
-        tipoPedidoAtualizado,
-        distanciaPedidoAtualizado,
-        pesoPedidoAtualizado,
-        valorBaseKmAtualizado,
-        valorBaseKgAtualizado,
-        valorDistanciaAtualizado,
-        valorPesoAtualizado,
-        acrescEntregaAtualizado,
-        taxaExtraAtualizado,
-        descEntregaAtualizado,
-        valorFinalAtualizado
-) => {
+  atualizarPedido: async (
+    idPedido,
+    idClienteAtualizado,
+    dataPedidoAtualizado,
+    tipoPedidoAtualizado,
+    distanciaPedidoAtualizado,
+    pesoPedidoAtualizado,
+    valorBaseKmAtualizado,
+    valorBaseKgAtualizado,
+    valorDistanciaAtualizado,
+    valorPesoAtualizado,
+    acrescEntregaAtualizado,
+    taxaExtraAtualizado,
+    descEntregaAtualizado,
+    valorFinalAtualizado
+  ) => {
     const pool = await getConnection();
-  const transaction = new sql.Transaction(pool);
-  await transaction.begin();
-  try {
-
-    const querySQL = `
+    const transaction = new sql.Transaction(pool);
+    await transaction.begin();
+    try {
+      const querySQL = `
       UPDATE Pedidos
       SET idCliente = @idCliente,
           dataPedido = @dataPedido,
@@ -159,8 +157,8 @@ atualizarPedido: async (
           valorBaseKg = @valorBaseKg,
           valorTotal = @valorTotal
       WHERE idPedido = @idPedido
-      `
-    const querySQLEntregas =`      
+      `;
+      const querySQLEntregas = `      
           UPDATE Entregas
           SET
           valorDistancia  = @valorDistancia,
@@ -172,42 +170,39 @@ atualizarPedido: async (
       WHERE idPedido = @idPedido
     `;
 
-    await transaction
-      .request()
-      .input("idPedido", sql.UniqueIdentifier, idPedido)
-      .input("idCliente", sql.UniqueIdentifier, idClienteAtualizado)
-      .input("dataPedido", sql.Date, dataPedidoAtualizado)
-      .input("tipoPedido", sql.VarChar(20), tipoPedidoAtualizado)
-      .input("distanciaPedido", sql.Decimal(10, 2), distanciaPedidoAtualizado)
-      .input("pesoPedido", sql.Decimal(10, 2), pesoPedidoAtualizado)
-      .input("valorBaseKm", sql.Decimal(10, 2), valorBaseKmAtualizado)
-      .input("valorBaseKg", sql.Decimal(10, 2), valorBaseKgAtualizado)
-      .input("valorTotal", sql.Decimal(10, 2), valorFinalAtualizado)
-      .query(querySQL);
+      await transaction
+        .request()
+        .input("idPedido", sql.UniqueIdentifier, idPedido)
+        .input("idCliente", sql.UniqueIdentifier, idClienteAtualizado)
+        .input("dataPedido", sql.Date, dataPedidoAtualizado)
+        .input("tipoPedido", sql.VarChar(20), tipoPedidoAtualizado)
+        .input("distanciaPedido", sql.Decimal(10, 2), distanciaPedidoAtualizado)
+        .input("pesoPedido", sql.Decimal(10, 2), pesoPedidoAtualizado)
+        .input("valorBaseKm", sql.Decimal(10, 2), valorBaseKmAtualizado)
+        .input("valorBaseKg", sql.Decimal(10, 2), valorBaseKgAtualizado)
+        .input("valorTotal", sql.Decimal(10, 2), valorFinalAtualizado)
+        .query(querySQL);
 
-    await transaction
-      .request()
-      .input("idPedido", sql.UniqueIdentifier, idPedido)
-      .input("valorDistancia", sql.Decimal(10, 2), valorDistanciaAtualizado)
-      .input("valorPeso", sql.Decimal(10, 2), valorPesoAtualizado)
-      .input("acrescEntrega", sql.Decimal(10, 2), acrescEntregaAtualizado)
-      .input("descEntrega", sql.Decimal(10, 2), descEntregaAtualizado)
-      .input("taxaExtra", sql.Decimal(10, 2), taxaExtraAtualizado)
-      .input("valorFinal", sql.Decimal(10, 2), valorFinalAtualizado)
-      .query(querySQLEntregas);
+      await transaction
+        .request()
+        .input("idPedido", sql.UniqueIdentifier, idPedido)
+        .input("valorDistancia", sql.Decimal(10, 2), valorDistanciaAtualizado)
+        .input("valorPeso", sql.Decimal(10, 2), valorPesoAtualizado)
+        .input("acrescEntrega", sql.Decimal(10, 2), acrescEntregaAtualizado)
+        .input("descEntrega", sql.Decimal(10, 2), descEntregaAtualizado)
+        .input("taxaExtra", sql.Decimal(10, 2), taxaExtraAtualizado)
+        .input("valorFinal", sql.Decimal(10, 2), valorFinalAtualizado)
+        .query(querySQLEntregas);
+    } catch (error) {
+      console.error("Erro ao atualizar pedido:", error);
+      throw error;
+    }
+  },
 
-  } catch (error) {
-    console.error("Erro ao atualizar pedido:", error);
-    throw error;
-  }
-},
-
-
-  
-statusPedido: async (idPedido, statusPedido, statusEntrega) => {
-  try {
-    const pool = await getConnection();
-    const querySQL = `
+  statusPedido: async (idPedido, statusPedido, statusEntrega) => {
+    try {
+      const pool = await getConnection();
+      const querySQL = `
       UPDATE Entregas
       SET statusEntrega = @statusEntrega
       WHERE idPedido = @idPedido;
@@ -217,18 +212,36 @@ statusPedido: async (idPedido, statusPedido, statusEntrega) => {
       WHERE idPedido = @idPedido;
     `;
 
-    await pool
-      .request()
-      .input("idPedido", sql.UniqueIdentifier, idPedido)
-      .input("statusPedido", sql.VarChar(11), statusPedido)
-      .input("statusEntrega", sql.VarChar(11), statusEntrega)
-      .query(querySQL);
+      await pool
+        .request()
+        .input("idPedido", sql.UniqueIdentifier, idPedido)
+        .input("statusPedido", sql.VarChar(11), statusPedido)
+        .input("statusEntrega", sql.VarChar(11), statusEntrega)
+        .query(querySQL);
+    } catch (error) {
+      console.error("Erro ao atualizar o pedido:", error);
+      throw error;
+    }
+  },
+  buscarEntrega: async (idPedido) => {
+    try {
+      const pool = await getConnection();
 
-  } catch (error) {
-    console.error("Erro ao atualizar o pedido:", error);
-    throw error;
-  }
-},
+      const querySQL = `   
+    SELECT * FROM Entregas
+    WHERE idPedido = @idPedido
+     `;
 
+      const result = await pool
+        .request()
+        .input("idPedido", sql.UniqueIdentifier, idPedido)
+        .query(querySQL);
+
+      return result.recordset;
+    } catch (error) {
+      console.error("Erro ao buscar entregas", error);
+      throw error;
+    }
+  },
 };
 module.exports = { pedidoModel };
